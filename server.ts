6a5db3fe4e736114e1ff6e7800e4c917e932ef76
Wojciech Chociej <w.chociej@cez.gov.pl>
2024-04-03 12:57:47 +0200

    isr translate build serve fix

--- server.ts
+++ server.ts
@@ -1,19 +1,29 @@
 import { CommonEngine } from '@angular/ssr';
 import express from 'express';
 import { fileURLToPath } from 'node:url';
-import { dirname, join, resolve } from 'node:path';
+import { dirname, basename, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
 import { ISRHandler } from '@rx-angular/isr/server';
+import { APP_BASE_HREF } from '@angular/common';
+import { LOCALE_ID } from '@angular/core';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
   const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-  const browserDistFolder = resolve(serverDistFolder, '../browser');
+
+  const lang = basename(serverDistFolder);
+  const langPath = `/${lang}/`;
+
+  const browserDistFolder = resolve(serverDistFolder, `../../browser/${lang}`);
   const indexHtml = join(serverDistFolder, 'index.server.html');
 
   const commonEngine = new CommonEngine({
     enablePerformanceProfiler: true,
+    providers:[
+      {provide:APP_BASE_HREF, useValue: langPath},
+      { provide: LOCALE_ID, useValue: lang },
+    ]
   });
 
   const isr = new ISRHandler({
@@ -45,22 +55,6 @@ export function app(): express.Express {
     })
   );
 
-  // All regular routes use the Angular engine
-  // server.get('*', (req, res, next) => {
-  //   const { protocol, originalUrl, baseUrl, headers } = req;
-  //
-  //   commonEngine
-  //     .render({
-  //       bootstrap,
-  //       documentFilePath: indexHtml,
-  //       url: `${protocol}://${headers.host}${originalUrl}`,
-  //       publicPath: browserDistFolder,
-  //       providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-  //     })
-  //     .then(html => res.send(html))
-  //     .catch(err => next(err));
-  // });
-
   server.get(
     '*',
     // Serve page if it exists in cache
@@ -73,14 +67,14 @@ export function app(): express.Express {
   return server;
 }
 
-function run(): void {
-  const port = process.env['PORT'] || 4000;
-
-  // Start up the Node server
-  const server = app();
-  server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
-}
-
-run();
+// function run(): void {
+//   const port = process.env['PORT'] || 4000;
+//
+//   // Start up the Node server
+//   const server = app();
+//   server.listen(port, () => {
+//     console.log(`Node Express server listening on http://localhost:${port}`);
+//   });
+// }
+//
+// run();
